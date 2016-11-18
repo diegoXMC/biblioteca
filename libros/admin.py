9516a54719d7b7a4186cd3ce92a5c96ab67a7dc3
diegoXMC <diegopalomila@yahoo.com.ar>
2016-11-18 09:19:41 -0600

    se modifico archivo admin y se craron carpetas estaticas y html

--- libros/admin.py
+++ libros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Pais, Editorial, Libro, Autor
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Pais)
+admin.site.register(Editorial)
+admin.site.register(Autor)
+admin.site.register(Libro)
